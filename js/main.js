1125fece5a2d8df93d4ffd22e3cf40ef2965ef05
seyed alireza mousavi <s.alireza.m8888@gmail.com>
2020-06-14 16:19:50 +0430

    complete navbar and header-slider style and add the font

--- js/main.js
+++ js/main.js
@@ -2,3 +2,16 @@ $(window).on("load", function () {
     $(".loading").fadeOut();
     console.log("fds")
 });
+
+$(window).resize(function () {
+    if ($(window).width() < 768) {
+        $('.navbar').removeClass('m-container');
+        $('.slider').removeClass('m-container');
+        $('.logo-section').addClass('left-60');
+    }
+    else {
+        $('.logo-section').removeClass('left-60');
+        $('.navbar').addClass('m-container');
+        $('.slider').addClass('m-container');
+    }
+});
